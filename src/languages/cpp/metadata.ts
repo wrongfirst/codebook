6b347b04d6b1b51151c8ab36a691f9b723b75ecd
Jitin <2485715+jitinnair1@users.noreply.github.com>
2026-08-29 14:53:05 +0530

    add cpp

---
+++ src/languages/cpp/metadata.ts
@@ -0,0 +1,10 @@
+import type { LanguageMetadata } from '../types';
+
+export const metadata: LanguageMetadata = {
+  id: 'cpp',
+  name: 'C++',
+  extension: '.cpp',
+  cmLanguage: 'cpp'
+};
+
+export default metadata;
